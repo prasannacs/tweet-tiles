afcb1949a9e38bce5d59dd1b23f96d0693377df6
prasannacs <prasannacs@gmail.com>
2022-10-13 15:53:41 -0400

    tile2

--- src/components/tile2.jsx
+++ src/components/tile2.jsx
@@ -16,13 +16,13 @@ export default function Tile2() {
 
       <head>
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:tile:template" content="1" />
+        <meta name="twitter:tile:template" content="2" />
         <meta property="twitter:site" content="@xcv" />
         <meta name="twitter:creator" content="@nickbilton" />
         <meta property="twitter:title" content="A Twitter for My Sister" />
         <meta property="twitter:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
         <meta property="twitter:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
-
+        <meta property="twitter:cta" content="A Button" />
       </head>
       <Container>
         <Row>
